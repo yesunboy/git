101b1b53bcb5349bce96a681496b00198984afd1
Daniel Ferreira <bnmvco@gmail.com>
2017-04-17 17:51:32 -0300

    dir_iterator: add tests for dir_iterator API
    Create t/helper/test-dir-iterator.c, which prints relevant
    information about a directory tree iterated over with dir_iterator.
    
    Create t/t0066-dir-iterator.sh, which tests that dir_iterator does
    iterate through a whole directory tree.
    
    Signed-off-by: Daniel Ferreira <bnmvco@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ t/helper/test-dir-iterator.c
@@ -0,0 +1,30 @@
+#include "git-compat-util.h"
+#include "strbuf.h"
+#include "iterator.h"
+#include "dir-iterator.h"
+
+int cmd_main(int argc, const char **argv)
+{
+	struct strbuf path = STRBUF_INIT;
+	struct dir_iterator *diter;
+
+	if (argc < 2)
+		die("BUG: test-dir-iterator needs one argument");
+
+	strbuf_add(&path, argv[1], strlen(argv[1]));
+
+	diter = dir_iterator_begin(path.buf);
+
+	while (dir_iterator_advance(diter) == ITER_OK) {
+		if (S_ISDIR(diter->st.st_mode))
+			printf("[d] ");
+		else if (S_ISREG(diter->st.st_mode))
+			printf("[f] ");
+		else
+			printf("[?] ");
+
+		printf("(%s) [%s] %s\n", diter->relative_path, diter->basename, diter->path.buf);
+	}
+
+	return 0;
+}
